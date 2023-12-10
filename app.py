68de2476d49769458af95f4a9a44a5fc776c74c5
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2023-12-10 17:54:22 +0700

    bisa else if

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ def predict():
     try:
         features = [float(request.form[x]) for x in request.form]
         prediction = model.predict([features])
-        return render_template("index.html", prediction_text="Kualitas fisik Anda: {}".format(prediction))
+        if prediction == 0:
+            return render_template("index.html", prediction_text="tidak berpengaruh", prediction=prediction)
+        elif prediction == 1:
+            return render_template("index.html", prediction_text="berpengaruh", prediction=prediction)
     except Exception as e:
         return render_template("index.html", prediction_text="Error: {}".format(str(e)))
 
